9d33ab45f9511eb397d505d1f78775cec640413f
chris crowd <chriscrowd@gmail.com>
2013-11-26 16:37:30 -0500

    home commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,26 +5,60 @@ class EmployeesController < ApplicationController
   def dashboard
   end
 
+
+  def list
+  	@employees = Employee.all
+  	
+  end
+
   #crud
 
   def new
+  	@employees = Employee.new
   end
 
   def show
+  	@employees = Employee.find(params[:id])
   end
 
   def edit
+  	@employees = Employee.find(params[:id])
   end
 
   def create
+
+  	@employees = Employee.new(get_params)
+
+  	if @employees.save
+
+  		redirect_to employees_dashboard_path
+  	else
+  		render 'new'
+  	end
+
   	
   end
 
   def update
+  		@employees = Employee.find(params[:id])
+
+  		if @employees.update(get_params)
+
+  			render 'show'
+
+  		else
+
+  			render 'edit'
+  		end
   	
   end
 
   def destroy
+  		@employees = Employee.find(params[:id])
+
+  		@employees.destroy
+
+  		redirect_to employees_dashboard_path
   	
   end
 
@@ -39,7 +73,7 @@ class EmployeesController < ApplicationController
 
   def get_params
 
-  	params.require(:employee).permit()
+  	params.require(:employee).permit(:first,:last,:middle,:birth,:city,:state,:street,:email,:phone,:employeeid,:hire)
   	
   end
 end
